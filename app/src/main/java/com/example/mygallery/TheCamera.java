6119ac8b2be6b2d64fb59707a897ff7c345cdc10
kevin <deadstone91@gmail.com>
2019-05-07 16:30:12 +0100

    added takePicture metod

--- app/src/main/java/com/example/mygallery/TheCamera.java
+++ app/src/main/java/com/example/mygallery/TheCamera.java
@@ -1,13 +1,21 @@
 package com.example.mygallery;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class TheCamera extends AppCompatActivity {
 
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_the_camera2);
+    }
+
+    private void takePicture(){
+        Intent 
+
     }
 }
